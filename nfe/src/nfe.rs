c0c468233335400dfe912d5779b42bbbae4d84c9
Luis Fernando Batels <luisfbatels@gmail.com>
2021-06-27 20:03:46 -0300

    Parse funcionando
    Parse da NF-e funcionando. Informações básicas da tag infNFe acessíveis

--- nfe/src/nfe.rs
+++ nfe/src/nfe.rs
@@ -4,6 +4,7 @@ use std::io::Read;
 use std::fs::File;
 use std::str::FromStr;
 use std::convert::TryFrom;
+use parsercher::{self, dom::*};
 
 /// Nota Fiscal Eletrônica
 pub struct Nfe {
@@ -12,10 +13,10 @@ pub struct Nfe {
 }
 
 /// Versão do layout da NF-e
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum VersaoLayout {
-    V4_00,
-    Outra
+    V4_00 = 4,
+    Outra = -1
 }
 
 impl FromStr for Nfe {
@@ -44,6 +45,39 @@ impl Nfe {
     /// Parse da NF-e a partir de uma string
     pub fn parse(s: &str) -> Result<Self, String> {
 
-        todo!()
+        let xml = parsercher::parse(s)
+            .map_err(|e| e.to_string())?;
+
+        // Saltamos direto para a tag <infNfe> já
+        // que se não houver essa tag, de nada nos
+        // adiantará a <NFe> ou <?xml>
+        let tags_infnfe = parsercher::search_tag(&xml, &Tag::new("infNFe"))
+            .ok_or("Tag <infNFe> não encontrada")?;
+        let infnfe = tags_infnfe.first()
+            .ok_or("Erro ao acessar a <infNFe>")?;
+
+        let chave_acesso = infnfe.get_attr("Id")
+            .ok_or("Atributo 'Id' não encontrado na tag <infNFe>")?
+            .replace("NFe", "");
+
+        let versao = infnfe.get_attr("versao")
+            .ok_or("Atributo 'versao' não encontrado na tag <infNFe>")?
+            .parse::<VersaoLayout>()?;
+
+        Ok(Self {
+            chave_acesso,
+            versao
+        })
+    }
+}
+
+impl FromStr for VersaoLayout {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "4.00" => VersaoLayout::V4_00,
+            _ => VersaoLayout::Outra
+        })
     }
 }
---
+++ nfe/src/tests/infnfe.rs
@@ -0,0 +1,19 @@
+//! Testes da tag <infNFe>
+
+use std::convert::TryFrom;
+use std::fs::File;
+
+use crate::*;
+
+#[test]
+fn from_read() -> Result<(), String> {
+
+    let f = File::open("xmls/nfe_layout4.xml")
+        .map_err(|e| e.to_string())?;
+    let nfe = Nfe::try_from(f)?;
+
+    assert_eq!("43180906929383000163550010000000261000010301", nfe.chave_acesso);
+    assert_eq!(VersaoLayout::V4_00, nfe.versao);
+
+    Ok(())
+}
