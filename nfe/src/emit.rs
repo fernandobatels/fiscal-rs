ee874ff4995a67c2992c1ae91711263b72f55e48
Luis Fernando Batels <luisfbatels@gmail.com>
2021-07-04 20:21:25 -0300

    Endereço do emitente
    Tag 'enderEmit' mapeada para o campo 'endereco'

--- nfe/src/emit.rs
+++ nfe/src/emit.rs
@@ -9,6 +9,19 @@ pub struct Emitente {
     pub nome_fantasia: Option<String>,
     pub ie: String,
     pub iest: Option<u32>,
+    pub endereco: Endereco,
+}
+
+pub struct Endereco {
+    pub logradouro: String,
+    pub numero: String,
+    pub complemento: Option<String>,
+    pub bairro: String,
+    pub codigo_municipio: u32,
+    pub nome_municipio: String,
+    pub sigla_uf: String,
+    pub cep: u32,
+    pub telefone: Option<String>
 }
 
 impl Emitente {
@@ -16,10 +29,10 @@ impl Emitente {
     /// Parse da seção <emit>
     pub(crate) fn parse(xml: &Dom) -> Result<Self, String> {
 
-        let mut t_ide = Dom::new(DomType::Tag);
-        t_ide.set_tag(Tag::new("emit"));
+        let mut t_emit = Dom::new(DomType::Tag);
+        t_emit.set_tag(Tag::new("emit"));
 
-        let emit = parsercher::search_dom(&xml, &t_ide)
+        let emit = parsercher::search_dom(&xml, &t_emit)
             .ok_or("Tag <emit> não encontrada")?;
 
         let cnpj = parsercher::search_text_from_tag_children(&emit, &Tag::new("CNPJ"))
@@ -51,12 +64,80 @@ impl Emitente {
             }
         };
 
+        let endereco = {
+
+            let mut t_endr = Dom::new(DomType::Tag);
+            t_endr.set_tag(Tag::new("enderEmit"));
+
+            let endr = parsercher::search_dom(&xml, &t_endr)
+                .ok_or("Tag <enderEmit> não encontrada")?;
+
+            let logradouro = parsercher::search_text_from_tag_children(&endr, &Tag::new("xLgr"))
+                .ok_or("Tag <xLgr> não encontrada na <enderEmit>")?[0]
+                .to_string();
+
+            let numero = parsercher::search_text_from_tag_children(&endr, &Tag::new("nro"))
+                .ok_or("Tag <nro> não encontrada na <enderEmit>")?[0]
+                .to_string();
+
+            let complemento = {
+                if let Some(comp) = parsercher::search_text_from_tag_children(&emit, &Tag::new("xCpl")) {
+                    Some(comp[0].to_string())
+                } else {
+                    None
+                }
+            };
+
+            let bairro = parsercher::search_text_from_tag_children(&endr, &Tag::new("xBairro"))
+                .ok_or("Tag <xBairro> não encontrada na <enderEmit>")?[0]
+                .to_string();
+
+            let codigo_municipio = parsercher::search_text_from_tag_children(&endr, &Tag::new("cMun"))
+                .ok_or("Tag <cMun> não encontrada na <enderEmit>")?[0]
+                .parse::<u32>()
+                .map_err(|e| e.to_string())?;
+
+            let nome_municipio = parsercher::search_text_from_tag_children(&endr, &Tag::new("xMun"))
+                .ok_or("Tag <xMun> não encontrada na <enderEmit>")?[0]
+                .to_string();
+
+            let sigla_uf = parsercher::search_text_from_tag_children(&endr, &Tag::new("UF"))
+                .ok_or("Tag <UF> não encontrada na <enderEmit>")?[0]
+                .to_string();
+
+            let cep = parsercher::search_text_from_tag_children(&endr, &Tag::new("CEP"))
+                .ok_or("Tag <CEP> não encontrada na <enderEmit>")?[0]
+                .parse::<u32>()
+                .map_err(|e| e.to_string())?;
+
+            let telefone = {
+                if let Some(fone) = parsercher::search_text_from_tag_children(&emit, &Tag::new("fone")) {
+                    Some(fone[0].to_string())
+                } else {
+                    None
+                }
+            };
+
+            Endereco {
+                logradouro,
+                numero,
+                complemento,
+                bairro,
+                codigo_municipio,
+                nome_municipio,
+                sigla_uf,
+                cep,
+                telefone
+            }
+        };
+
         Ok(Emitente {
             cnpj,
             razao_social,
             nome_fantasia,
             ie,
-            iest
+            iest,
+            endereco
         })
     }
 }
--- nfe/src/tests/emit.rs
+++ nfe/src/tests/emit.rs
@@ -20,3 +20,24 @@ fn base() -> Result<(), String> {
 
     Ok(())
 }
+
+
+#[test]
+fn endereco() -> Result<(), String> {
+
+    let f = File::open("xmls/nfe_layout4.xml")
+        .map_err(|e| e.to_string())?;
+    let endereco = Nfe::try_from(f)?.emit.endereco;
+
+    assert_eq!("Rua dos Testes", endereco.logradouro);
+    assert_eq!("1020", endereco.numero);
+    assert_eq!(Some("0".to_string()), endereco.complemento);
+    assert_eq!("Centro", endereco.bairro);
+    assert_eq!(4319901, endereco.codigo_municipio);
+    assert_eq!("SAPIRANGA", endereco.nome_municipio);
+    assert_eq!("RS", endereco.sigla_uf);
+    assert_eq!(93800000, endereco.cep);
+    assert_eq!(Some("5190909090".to_string()), endereco.telefone);
+
+    Ok(())
+}
