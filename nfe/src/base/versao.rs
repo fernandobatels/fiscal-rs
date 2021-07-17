55a965a04d6417eb3e9ca511f88dc6dab374d96a
Luis Fernando Batels <luisfbatels@gmail.com>
2021-07-17 10:57:31 -0300

    Suporte ao layout 4
    Crate ajustada para apenas suportar, ao menos por enquanto, o
    layout 4.

--- nfe/src/base/versao.rs
+++ nfe/src/base/versao.rs
@@ -5,17 +5,16 @@ use std::str::FromStr;
 /// Versão do layout da NF-e
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum VersaoLayout {
-    V4_00 = 4,
-    Outra = -1
+    V4_00 = 4
 }
 
 impl FromStr for VersaoLayout {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(match s {
-            "4.00" => VersaoLayout::V4_00,
-            _ => VersaoLayout::Outra
-        })
+        match s {
+            "4.00" => Ok(VersaoLayout::V4_00),
+            _ => Err(format!("Layout {} ainda não suportado", s))
+        }
     }
 }
